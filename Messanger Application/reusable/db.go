61321768125178e8bfaf69674f3d9bb52b9941ab
GrandLaite <53825459+GrandLaite@users.noreply.github.com>
2024-11-06 07:27:33 +0600

    Update db.go

--- Messanger Application/reusable/db.go
+++ Messanger Application/reusable/db.go
@@ -4,19 +4,16 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/jackc/pgx/v5/stdlib" // Драйвер для PostgreSQL через pgx
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-// Функция для подключения к базе данных PostgreSQL
 func ConnectDB() (*sql.DB, error) {
-	// Строка подключения к базе данных
-	dsn := "postgres://postgres:1@localhost:5432/gol_messenger" // Укажите свои параметры
+	dsn := "postgres://postgres:1@localhost:5432/gol_messenger" 
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database connection: %w", err)
 	}
 
-	// Проверка соединения
 	err = db.Ping()
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
