31731e5506f192abc64301609b8bf10c5ac8bac0
GrandLaite <53825459+GrandLaite@users.noreply.github.com>
2024-11-14 11:07:47 +0600

    Update main.go

--- Hello World Server/main.go
+++ Hello World Server/main.go
@@ -9,7 +9,30 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World!")
 }
 
+func connectDB() (*sql.DB, error) {
+	dsn := "postgres://postgres:1@localhost:5432/gol_messenger"
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось авторизовать соединение с базой данных: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("не удалось подключиться к базе данных: %w", err)
+	}
+
+	fmt.Println("Подключение к базе данных прошло успешно")
+	return db, nil
+}
+
 func main() {
+	db, err := connectDB()
+	if err != nil {
+		fmt.Printf("Ошибка подключения к базе данных: %v\n", err)
+		return
+	}
+	defer db.Close()
+	
 	http.HandleFunc("/", helloHandler)
 	fmt.Println("Сервер запущен и доступен по адресу http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
